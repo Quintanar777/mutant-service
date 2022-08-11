c7e6633e1ee27e5c0482a877c3fef5795fb53761
Erick Quintanar <quintanar777@gmail.com>
2022-08-11 13:17:01 -0500

    Primer commit

---
+++ src/main/java/mx/meli/mutantservice/MutantServiceApplication.java
@@ -0,0 +1,13 @@
+package mx.meli.mutantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/meli/mutantservice/MutantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.meli.mutantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
